7247484d8d5184997c7efcf6889b3f69e908f0e9
ides784 <67028041+ides784@users.noreply.github.com>
2022-01-21 12:50:16 -0500

    Delete gittest2.java

--- gittest2.java
+++
@@ -1,9 +0,0 @@
-public class gittest2 {
-    public static void main(String args[]) {
-        int x=10;
-        int y=25;
-        int z=x+y;
-
-        System.out.println("test 2 " + z);
-    }
-}
